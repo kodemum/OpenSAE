01d2530ac47884639e0652e9142ef984446d20f1
Magnus Flansmose Madsen <mum@kodeministeriet.dk>
2022-07-18 00:42:40 +0200

    Show points for all symbol vertices

---
+++ OpenSAE/Converters/NumberOffsetConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace OpenSAE.Converters
+{
+    internal class NumberOffsetConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is short && parameter is string arg && short.TryParse(arg, out short offset))
+            {
+                return (short)value + offset;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
