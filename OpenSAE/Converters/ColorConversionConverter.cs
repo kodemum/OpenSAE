6caf1a1d7ac8820bc88de0012b8bbc3e78c9e739
Magnus Flansmose Madsen <mum@kodeministeriet.dk>
2022-07-27 00:47:28 +0200

    Replace color mapping shader with color converter
    Looks better, more intuitive and should be faster

---
+++ OpenSAE/Converters/ColorConversionConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace OpenSAE.Converters
+{
+    internal class ColorConversionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == DependencyProperty.UnsetValue)
+                return DependencyProperty.UnsetValue;
+
+            if (value is not Color color)
+                throw new ArgumentException(nameof(value));
+
+            return Core.SymbolArtColorHelper.ApplyCurve(color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == DependencyProperty.UnsetValue)
+                return DependencyProperty.UnsetValue;
+
+            if (value is not Color color)
+                throw new ArgumentException(nameof(value));
+
+            return Core.SymbolArtColorHelper.RemoveCurve(color);
+        }
+    }
+}
