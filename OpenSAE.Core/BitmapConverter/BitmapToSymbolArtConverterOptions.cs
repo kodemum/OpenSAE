f51f4ba1f0660a1d44a46fae5b6c2f4ea7ab72f7
Magnus Flansmose Madsen <mum@kodeministeriet.dk>
2022-10-13 19:07:48 +0200

    Update default converter values

--- OpenSAE.Core/BitmapConverter/BitmapToSymbolArtConverterOptions.cs
+++ OpenSAE.Core/BitmapConverter/BitmapToSymbolArtConverterOptions.cs
@@ -2,9 +2,9 @@
 {
     public class BitmapToSymbolArtConverterOptions
     {
-        public double SizeXOffset { get; set; } = 0.65;
+        public double SizeXOffset { get; set; } = 0.35;
 
-        public double SizeYOffset { get; set; } = 0.5;
+        public double SizeYOffset { get; set; } = 0.3;
 
         public int ResizeImageHeight { get; set; } = 24;
 
@@ -14,9 +14,9 @@
 
         public bool DisableLayering { get; set; } = false;
 
-        public double OffsetSizeYExponent { get; set; } = 0.9;
+        public double OffsetSizeYExponent { get; set; } = 0.75;
 
-        public double OffsetSizeXExponent { get; set; } = 1;
+        public double OffsetSizeXExponent { get; set; } = 0.7;
 
         public double CenterYOffset { get; set; } = 0.25;
 
